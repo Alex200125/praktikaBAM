89965d1b6c16a7a5049d27fd49d4a2263593f5e7
Alex <example@gmail.com>
2020-08-27 21:07:10 +0400

    add scroll (pageup)

--- src/js/script.js
+++ src/js/script.js
@@ -173,4 +173,22 @@ $(document).ready(function() {
     });
 
     //php forms email end
+
+    // pageup scroll
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+    
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    // pageup scroll end
 });
